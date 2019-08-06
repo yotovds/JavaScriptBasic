1d32b7612b83f5cc9d7a3cb3969d1d5f6f4e8d71
Dimitar Yotov <yotovds@gmail.com>
2019-08-06 16:48:55 +0300

    https://judge.softuni.bg/Contests/Practice/Index/1010#0

---
+++ 001-FirstSteps/nums-1-To-20.js
@@ -0,0 +1,6 @@
+function nums1To20(){
+    for (let i = 1; i <= 20; i++) {
+        console.log(i);        
+    }
+}
+//nums1To20();
---
+++ 001-FirstSteps/rectangleArea .js
@@ -0,0 +1,9 @@
+function rectangleArea (input){
+    let a = Number(input.shift());
+    let b = Number(input.shift());
+    
+    let area = a * b;
+
+    console.log(area);
+}
+rectangleArea([2, 7]);
